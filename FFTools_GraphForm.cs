a016d8a9f25f2d95a0494ad6596f2756a1ed8cfa
Ran Duan <rnduan@gmail.com>
2014-10-01 17:59:24 -0500

    Added dynamic window sizing depending on mineral deposits.

--- FFTools_GraphForm.cs
+++ FFTools_GraphForm.cs
@@ -14,21 +14,22 @@ namespace FFTools {
             formStartThread.Start();
 
             List<MineralDeposit> mdlist = new List<MineralDeposit>();
-            mdlist.Add(new MineralDeposit(false, (float)-111.6122, (float)0, (float)164.0807));
-            mdlist.Add(new MineralDeposit(true, (float)-99.16898, (float)0, (float)158.4617));
-            mdlist.Add(new MineralDeposit(false, (float)-117.896, (float)0, (float)168.2807));
-            mdlist.Add(new MineralDeposit(false, (float)-89.49954, (float)0, (float)13.95296));
-            mdlist.Add(new MineralDeposit(false, (float)-88.62637, (float)0, (float)24.27498));
-            mdlist.Add(new MineralDeposit(false, (float)-30.22703, (float)0, (float)19.45272));
-            mdlist.Add(new MineralDeposit(true, (float)-86.37704, (float)0, (float)32.77007));
-            mdlist.Add(new MineralDeposit(false, (float)-23.19106, (float)0, (float)29.00829));
-            mdlist.Add(new MineralDeposit(true, (float)-24.8432, (float)0, (float)39.98703));
-            mdlist.Add(new MineralDeposit(false, (float)-46.58563, (float)0, (float)68.71722));
-            mdlist.Add(new MineralDeposit(false, (float)-101.0807, (float)0, (float)79.47763));
-            mdlist.Add(new MineralDeposit(false, (float)-46.45711, (float)0, (float)84.50039));
-            mdlist.Add(new MineralDeposit(true, (float)-45.5108, (float)0, (float)78.73927));
-            mdlist.Add(new MineralDeposit(false, (float)-97.96165, (float)0, (float)92.54741));
-            mdlist.Add(new MineralDeposit(true, (float)-98.66853, (float)0, (float)87.56786));
+            mdlist.Add(new MineralDeposit(false, (float)205.1436, 0, (float)-83.35779));
+            mdlist.Add(new MineralDeposit(false, (float)216.1513, 0, (float)-87.30682));
+            mdlist.Add(new MineralDeposit(false, (float)221.7113, 0, (float)-95.18837));
+            mdlist.Add(new MineralDeposit(false, (float)225.8484, 0, (float)-106.7841));
+            mdlist.Add(new MineralDeposit(false, (float)256.3488, 0, (float)-215.9667));
+            mdlist.Add(new MineralDeposit(false, (float)262.8185, 0, (float)-170.5062));
+            mdlist.Add(new MineralDeposit(false, (float)274.6811, 0, (float)-247.5));
+            mdlist.Add(new MineralDeposit(false, (float)286.9102, 0, (float)-252.5938));
+            mdlist.Add(new MineralDeposit(false, (float)317.0013, 0, (float)-178.881));
+            mdlist.Add(new MineralDeposit(false, (float)323.3648, 0, (float)-182.2007));
+            mdlist.Add(new MineralDeposit(false, (float)325.8448, 0, (float)-265.9896));
+            mdlist.Add(new MineralDeposit(false, (float)333.5263, 0, (float)-214.3547));
+            mdlist.Add(new MineralDeposit(false, (float)334.8407, 0, (float)-242.5161));
+            mdlist.Add(new MineralDeposit(true , (float)332.7316, 0, (float)-256.8401));
+            mdlist.Add(new MineralDeposit(true, (float)261.0139, 0, (float)-202.0589));
+            mdlist.Add(new MineralDeposit(true, (float)291.211, 0, (float)-255.4915));
             TheGraphForm.setViewMinDepList(mdlist);
 
             while (true) {
@@ -44,11 +45,17 @@ namespace FFTools {
         }
 
 
-        private const int WIN_WIDTH = 650;
-        private const int WIN_HEIGHT = 600;
-        private const int GRID_ILMS_UNITS = 5;
-        private const int ILMS_PADDING = 10;
-        private const int PIXELS_PER_ILMS = 5;
+        private const int BITMAP_SIZE_IN_PIXELS = 2200;
+        private const int BITMAP_OFFSET_TO_ORIGIN = BITMAP_SIZE_IN_PIXELS / 2;
+        private const int GRID_PADDING_IN_PIXELS = 100;
+        private const int GRID_SPACING_IN_EILMS = 5;
+        private const int GRID_PIXELS_PER_ILMS = 2;
+        private const int GRID_COLOR_LINES_R = 0x30;
+        private const int GRID_COLOR_LINES_G = 0x30;
+        private const int GRID_COLOR_LINES_B = 0x30;
+        private const int GRID_COLOR_MINDEP_R = 0xF0;
+        private const int GRID_COLOR_MINDEP_G = 0xF0;
+        private const int GRID_COLOR_MINDEP_B = 0xFF;
         private System.Windows.Forms.Timer RefreshTimer;
         private IContainer Components;
         private Object DataLock = new Object();
@@ -60,8 +67,8 @@ namespace FFTools {
             this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
             this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
             this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-            this.Size = new Size(WIN_WIDTH, WIN_HEIGHT);
-            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Size = new Size(500, 500);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             ViewMinDepList = new List<MineralDeposit>();
             ViewPlayer = new Player(0, 0, 0, 0);
         }
@@ -82,73 +89,93 @@ namespace FFTools {
             base.OnLoad(e);
             RefreshTimer.Start();
         }
+        // Draw on a 1000px x 1000px bitmap in memory where the x-axis is at y=500px, and the y-axis is at x=500px.
+        // Then the section of the bitmap is shown on the form.
         protected override void OnPaint(PaintEventArgs e) {
             base.OnPaint(e);
 
-            Bitmap bmp = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Bitmap bmp = new Bitmap(BITMAP_SIZE_IN_PIXELS, BITMAP_SIZE_IN_PIXELS, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             Graphics gBmp = Graphics.FromImage(bmp);
             gBmp.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
 
-            paintGrid(gBmp);
-            paintMineralDeposits(gBmp);
-            paintPlayer(gBmp);
-            //paintText(gBmp);
-
-            Graphics gForm = e.Graphics;
-            gForm.FillRectangle(Brushes.Black, this.ClientRectangle);
-            gForm.DrawImage(bmp, 0, 0, bmp.Width, bmp.Height);
-        }
-        private void paintGrid(Graphics gBmp) {
-            int pixels_per_grid_unit = GRID_ILMS_UNITS * PIXELS_PER_ILMS;
-            Pen gridPen = new Pen(Color.FromArgb(0x30, 0x30, 0x30));
-            // Vertical lines.
-            for (int x = 0; x < this.ClientRectangle.Width; x += pixels_per_grid_unit) {
-                gBmp.DrawLine(gridPen, x, 0, x, this.ClientRectangle.Height);
-            }
-            // Horizontal lines.
-            for (int y = 0; y < this.ClientRectangle.Height; y += pixels_per_grid_unit) {
-                gBmp.DrawLine(gridPen, 0, y, this.ClientRectangle.Width, y);
-            }
-            gridPen.Dispose();
-        }
-        private void paintMineralDeposits(Graphics gBmp) {
+            // Grab a copy of semaphored view data.
             List<MineralDeposit> tmpViewMinDepList = new List<MineralDeposit>();
+            Player tmpViewPlayer;
             lock (DataLock) {
                 foreach (MineralDeposit vmd in ViewMinDepList) {
                     tmpViewMinDepList.Add(new MineralDeposit(vmd.vis, vmd.x, vmd.z, vmd.y));
                 }
+                tmpViewPlayer = new Player(ViewPlayer.x, ViewPlayer.z, ViewPlayer.y, ViewPlayer.rot);
             }
-//            // Find shift to orgin offset.
-//            float shiftx = Single.MaxValue;
-//            float shifty = Single.MaxValue;
-//            foreach (MineralDeposit tvmd in tmpViewMinDepList) {
-//                if (Math.Abs(tvmd.x) < Math.Abs(shiftx)) shiftx = tvmd.x;
-//                if (Math.Abs(tvmd.y) < Math.Abs(shifty)) shifty = tvmd.y;
-//            }
-//            // Pad shift offset.
-//            if (shiftx < 0) shiftx += ILMS_PADDING;
-//            else shiftx -= ILMS_PADDING;
-//            if (shifty < 0) shifty += ILMS_PADDING;
-//            else shifty -= ILMS_PADDING;
-            // Draw mineral deposits.
+
+            paintGrid(gBmp);
+            paintMineralDeposits(gBmp, tmpViewMinDepList);
+            paintPlayer(gBmp, tmpViewPlayer);
+            //paintText(gBmp);
+
+            // Find top-left, bottom-right in aboslute ilms of mineral deposits.
+            // Convert to absolute pixels for bitmap.
+            // Resize form accoridingly and draw.
+            float smallestX = Single.MaxValue;
+            float smallestY = Single.MaxValue;
+            float largestX = Single.MinValue;
+            float largestY = Single.MinValue;
             foreach (MineralDeposit tvmd in tmpViewMinDepList) {
-                gBmp.FillEllipse(Brushes.AliceBlue, 
-                    Math.Abs(tvmd.x) * PIXELS_PER_ILMS, 
-                    Math.Abs(tvmd.y) * PIXELS_PER_ILMS,
+                if (tvmd.x < smallestX) smallestX = tvmd.x;
+                if (tvmd.y < smallestY) smallestY = tvmd.y;
+                if (tvmd.x > largestX) largestX = tvmd.x;
+                if (tvmd.y > largestY) largestY = tvmd.y;
+            }
+            int bitmapSmallestX = (int)Math.Round(smallestX * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
+            int bitmapSmallestY = (int)Math.Round(smallestY * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
+            int bitmapLargestX = (int)Math.Round(largestX * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
+            int bitmapLargestY = (int)Math.Round(largestY * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
+
+            this.Size = new Size(bitmapLargestX - bitmapSmallestX + (2*GRID_PADDING_IN_PIXELS),
+                                 bitmapLargestY - bitmapSmallestY + (2*GRID_PADDING_IN_PIXELS));
+            Graphics gForm = e.Graphics;
+            gForm.FillRectangle(Brushes.Black, 0, 0, 
+                                bitmapLargestX - bitmapSmallestX + (2*GRID_PADDING_IN_PIXELS),
+                                bitmapLargestY - bitmapSmallestY + (2*GRID_PADDING_IN_PIXELS));
+            RectangleF desRect = new RectangleF(0, 0,
+                                                bitmapLargestX - bitmapSmallestX + (2*GRID_PADDING_IN_PIXELS),
+                                                bitmapLargestY - bitmapSmallestY + (2*GRID_PADDING_IN_PIXELS));
+            RectangleF srcRect = new RectangleF(bitmapSmallestX - GRID_PADDING_IN_PIXELS,
+                                                bitmapSmallestY - GRID_PADDING_IN_PIXELS,
+                                                bitmapLargestX - bitmapSmallestX + (2*GRID_PADDING_IN_PIXELS),
+                                                bitmapLargestY - bitmapSmallestY + (2*GRID_PADDING_IN_PIXELS));
+            gForm.DrawImage(bmp, desRect, srcRect, GraphicsUnit.Pixel);
+        }
+        private void paintGrid(Graphics gBmp) {
+            int pixels_per_grid_unit = GRID_SPACING_IN_EILMS * GRID_PIXELS_PER_ILMS;
+            Pen gridPen = new Pen(Color.FromArgb(GRID_COLOR_LINES_R,
+                                                 GRID_COLOR_LINES_G,
+                                                 GRID_COLOR_LINES_B));
+            // Vertical lines.
+            for (int x = 0; x < BITMAP_SIZE_IN_PIXELS; x += pixels_per_grid_unit) {
+                gBmp.DrawLine(gridPen, x, 0, x, BITMAP_SIZE_IN_PIXELS);
+            }
+            // Horizontal lines.
+            for (int y = 0; y < BITMAP_SIZE_IN_PIXELS; y += pixels_per_grid_unit) {
+                gBmp.DrawLine(gridPen, 0, y, BITMAP_SIZE_IN_PIXELS, y);
+            }
+            gridPen.Dispose();
+        }
+        private void paintMineralDeposits(Graphics gBmp, List<MineralDeposit> tmpViewMinDepList) {
+            SolidBrush mdBrush = new SolidBrush(Color.FromArgb(GRID_COLOR_MINDEP_R,
+                                                               GRID_COLOR_MINDEP_G,
+                                                               GRID_COLOR_MINDEP_B));
+            foreach (MineralDeposit tvmd in tmpViewMinDepList) {
+                gBmp.FillEllipse(mdBrush, 
+                    (int)Math.Round(tvmd.x * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN, 
+                    (int)Math.Round(tvmd.y * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN,
                     6, 6);
             }
         }
-        private void paintPlayer(Graphics gBmp) {
-            Player tvp;
-            lock (DataLock) {
-                tvp = new Player(ViewPlayer.x,
-                    ViewPlayer.z,
-                    ViewPlayer.y,
-                    ViewPlayer.rot);
-            }
+        private void paintPlayer(Graphics gBmp, Player tmpViewPlayer) {
             gBmp.FillEllipse(Brushes.Crimson,
-                Math.Abs(tvp.x) * PIXELS_PER_ILMS,
-                Math.Abs(tvp.y) * PIXELS_PER_ILMS,
+                (int)Math.Round(tmpViewPlayer.x * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN,
+                (int)Math.Round(tmpViewPlayer.y * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN,
                 10, 10);
         }
         private void paintText(Graphics gBmp) {
