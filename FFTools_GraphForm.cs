f5cadfc4dcf9ee529c6954fbf2778b7cec03ab9f
Ran Duan <rnduan@gmail.com>
2014-10-01 18:28:55 -0500

    Added an exception for null mineral deposit lists

--- FFTools_GraphForm.cs
+++ FFTools_GraphForm.cs
@@ -14,22 +14,22 @@ namespace FFTools {
             formStartThread.Start();
 
             List<MineralDeposit> mdlist = new List<MineralDeposit>();
-            mdlist.Add(new MineralDeposit(false, (float)205.1436, 0, (float)-83.35779));
-            mdlist.Add(new MineralDeposit(false, (float)216.1513, 0, (float)-87.30682));
-            mdlist.Add(new MineralDeposit(false, (float)221.7113, 0, (float)-95.18837));
-            mdlist.Add(new MineralDeposit(false, (float)225.8484, 0, (float)-106.7841));
-            mdlist.Add(new MineralDeposit(false, (float)256.3488, 0, (float)-215.9667));
-            mdlist.Add(new MineralDeposit(false, (float)262.8185, 0, (float)-170.5062));
-            mdlist.Add(new MineralDeposit(false, (float)274.6811, 0, (float)-247.5));
-            mdlist.Add(new MineralDeposit(false, (float)286.9102, 0, (float)-252.5938));
-            mdlist.Add(new MineralDeposit(false, (float)317.0013, 0, (float)-178.881));
-            mdlist.Add(new MineralDeposit(false, (float)323.3648, 0, (float)-182.2007));
-            mdlist.Add(new MineralDeposit(false, (float)325.8448, 0, (float)-265.9896));
-            mdlist.Add(new MineralDeposit(false, (float)333.5263, 0, (float)-214.3547));
-            mdlist.Add(new MineralDeposit(false, (float)334.8407, 0, (float)-242.5161));
-            mdlist.Add(new MineralDeposit(true , (float)332.7316, 0, (float)-256.8401));
-            mdlist.Add(new MineralDeposit(true, (float)261.0139, 0, (float)-202.0589));
-            mdlist.Add(new MineralDeposit(true, (float)291.211, 0, (float)-255.4915));
+            //mdlist.Add(new MineralDeposit(false, (float)205.1436, 0, (float)-83.35779));
+            //mdlist.Add(new MineralDeposit(false, (float)216.1513, 0, (float)-87.30682));
+            //mdlist.Add(new MineralDeposit(false, (float)221.7113, 0, (float)-95.18837));
+            //mdlist.Add(new MineralDeposit(false, (float)225.8484, 0, (float)-106.7841));
+            //mdlist.Add(new MineralDeposit(false, (float)256.3488, 0, (float)-215.9667));
+            //mdlist.Add(new MineralDeposit(false, (float)262.8185, 0, (float)-170.5062));
+            //mdlist.Add(new MineralDeposit(false, (float)274.6811, 0, (float)-247.5));
+            //mdlist.Add(new MineralDeposit(false, (float)286.9102, 0, (float)-252.5938));
+            //mdlist.Add(new MineralDeposit(false, (float)317.0013, 0, (float)-178.881));
+            //mdlist.Add(new MineralDeposit(false, (float)323.3648, 0, (float)-182.2007));
+            //mdlist.Add(new MineralDeposit(false, (float)325.8448, 0, (float)-265.9896));
+            //mdlist.Add(new MineralDeposit(false, (float)333.5263, 0, (float)-214.3547));
+            //mdlist.Add(new MineralDeposit(false, (float)334.8407, 0, (float)-242.5161));
+            //mdlist.Add(new MineralDeposit(true , (float)332.7316, 0, (float)-256.8401));
+            //mdlist.Add(new MineralDeposit(true, (float)261.0139, 0, (float)-202.0589));
+            //mdlist.Add(new MineralDeposit(true, (float)291.211, 0, (float)-255.4915));
             TheGraphForm.setViewMinDepList(mdlist);
 
             while (true) {
@@ -116,10 +116,10 @@ namespace FFTools {
             // Find top-left, bottom-right in aboslute ilms of mineral deposits.
             // Convert to absolute pixels for bitmap.
             // Resize form accoridingly and draw.
-            float smallestX = Single.MaxValue;
-            float smallestY = Single.MaxValue;
-            float largestX = Single.MinValue;
-            float largestY = Single.MinValue;
+            float smallestX = BITMAP_OFFSET_TO_ORIGIN;
+            float smallestY = BITMAP_OFFSET_TO_ORIGIN;
+            float largestX = -BITMAP_OFFSET_TO_ORIGIN;
+            float largestY = -BITMAP_OFFSET_TO_ORIGIN;
             foreach (MineralDeposit tvmd in tmpViewMinDepList) {
                 if (tvmd.x < smallestX) smallestX = tvmd.x;
                 if (tvmd.y < smallestY) smallestY = tvmd.y;
@@ -130,6 +130,14 @@ namespace FFTools {
             int bitmapSmallestY = (int)Math.Round(smallestY * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
             int bitmapLargestX = (int)Math.Round(largestX * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
             int bitmapLargestY = (int)Math.Round(largestY * GRID_PIXELS_PER_ILMS) + BITMAP_OFFSET_TO_ORIGIN;
+
+            // If there 1 or fewer mineral deposits, default to a size.
+            if (tmpViewMinDepList.Count < 2) {
+                bitmapSmallestX = BITMAP_OFFSET_TO_ORIGIN - 200;
+                bitmapSmallestY = BITMAP_OFFSET_TO_ORIGIN - 200;
+                bitmapLargestX = BITMAP_OFFSET_TO_ORIGIN + 200;
+                bitmapLargestY = BITMAP_OFFSET_TO_ORIGIN + 200;
+            }
 
             this.Size = new Size(bitmapLargestX - bitmapSmallestX + (2*GRID_PADDING_IN_PIXELS),
                                  bitmapLargestY - bitmapSmallestY + (2*GRID_PADDING_IN_PIXELS));
