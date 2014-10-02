b4163bd92110f99943107d93b3324ac0c201b5e1
Stephen Wang <stephenwang.us@gmail.com>
2014-10-02 17:39:35 -0500

    NavigatorGraph constructor given array of Locations Waypoint no longer extends Location, but contains a Location as a property

--- FFTools_NavigatorGraph.cs
+++ FFTools_NavigatorGraph.cs
@@ -3,14 +3,72 @@ using System.Collections.Generic;
 
 namespace FFTools {
 	public class NavigatorGraph {
-		enum MoveDirection {NTOS, STON, ETOW, WTOE};
-
+		private const float DIST_PER_GRID = 5;
+		private const int BUFFER_MULTIPLIER = 3;	//BUFFER_MULTIPLIER * DIST_PER_GRID is buffer space on edges of graph
 		public Waypoint [][] NavGraph; //apparently jagged arrays [][] are faster than multidimensional [,]?
 
-		public NavigatorGraph (int width, int height) {
-			this.NavGraph = new Waypoint[height][] ;
-			for (int y = 0; y < height; y++) {
-				this.NavGraph[y] = new Waypoint[width];
+		public NavigatorGraph (Location[] locations) {
+			//find dimensions in "in-game" units to hold all locations
+			float totalX = 0, totalY = 0;
+
+			//most extreme "in-game" X, Y coordinates -- used to fill each grid
+			float negX = 0, negY = 0, posX = 0, posY = 0;
+
+			foreach (Location location in locations) {
+				if (location.x < negX) negX = location.x;
+				if (location.y < negY) negY = location.y;
+				if (location.x > posX) posX = location.x;
+				if (location.y > posY) posY = location.y;
+			}
+
+			//round "in-game" units to nearest DIST_PER_GRID multiple
+			totalX = totalX + DIST_PER_GRID - (totalX % DIST_PER_GRID);
+			totalY = totalY + DIST_PER_GRID - (totalY % DIST_PER_GRID);
+			if(negY > 0)
+				negY = negY - (negY % DIST_PER_GRID);
+			else
+				negY = negY - (DIST_PER_GRID - (negY % DIST_PER_GRID));
+
+			if(negX > 0)
+				negX = negX - (negX % DIST_PER_GRID);
+			else
+				negX = negX - (DIST_PER_GRID - (negX % DIST_PER_GRID));
+
+			if(posY > 0)
+				posY = posY - (posY % DIST_PER_GRID);
+			else
+				posY = posY - (DIST_PER_GRID - (posY % DIST_PER_GRID));
+
+			if(posX > 0)
+				posX = posX - (posX % DIST_PER_GRID);
+			else
+				posX = posX - (DIST_PER_GRID - (posX % DIST_PER_GRID));
+
+			//calculate dimensions in "in-game" units needed and add buffer on all sides
+			totalX = posX - negX + 2*BUFFER_MULTIPLIER*DIST_PER_GRID;
+			totalY = posY - posY + 2*BUFFER_MULTIPLIER*DIST_PER_GRID;
+
+			//# of grid square things
+			int graphWidth = (int) (totalX/DIST_PER_GRID);
+			int graphHeight = (int) (totalY/DIST_PER_GRID);
+
+			//create NavGraph
+			this.NavGraph = new Waypoint[graphHeight][];
+			for (int y = 0; y < graphHeight; y++) {
+				this.NavGraph[y] = new Waypoint[graphWidth];
+			}
+
+			//initial "in-game" coordinates stored in [0][0] = most negative X, most positive Y + buffers applied
+			float graphX = negX - BUFFER_MULTIPLIER*DIST_PER_GRID + DIST_PER_GRID/2;
+			float graphY = posY + BUFFER_MULTIPLIER*DIST_PER_GRID - DIST_PER_GRID/2;
+
+			//fill in all grids
+			for (int y = 0; y < graphHeight; y++) {
+				for (int x = 0; x < graphWidth; x++) {
+					this.NavGraph[y][x] = new Waypoint (graphX, graphY, 0, true, true, true, true);	//NavGraph Z coordinates aren't used
+					graphX = graphX + DIST_PER_GRID;
+				}
+				graphY = graphY + DIST_PER_GRID;
 			}
 		}
 
@@ -33,7 +91,31 @@ namespace FFTools {
 		}
 
 		public void markObstacle (int x, int y, MoveDirection direction) {
-			this.NavGraph[y][x].
+			this.NavGraph[y][x].canTravelFrom[(int) direction] = false;
+		}
+
+		//from and to coordinates should be adjacent
+		public bool canNavigate (int fromX, int fromY, int toX, int toY) {
+			if (fromY == toY) { //EtoW or WtoE
+				int xDir = fromX - toX;
+				if(Math.Abs(xDir) > 1) return false; //not adjacent
+				if(xDir == 0) return true;			 //same coordinates, technically true
+				if(xDir > 0) 	//EtoW
+					return this.NavGraph[toY][toX].canTravelFrom[(int)MoveDirection.EtoW];
+				else			//WtoE
+					return this.NavGraph[toY][toX].canTravelFrom[(int)MoveDirection.WtoE];
+			}
+
+			if (fromX == toX) { //NtoS or NtoS
+				int yDir = fromY - toY;
+				if(Math.Abs(yDir) > 1) return false; //not adjacent
+				if(yDir == 0) return true;			 //same coordinates, technically true
+				if(yDir > 0) 	//StoN
+					return this.NavGraph[toY][toX].canTravelFrom[(int)MoveDirection.StoN];
+				else			//NtoS
+					return this.NavGraph[toY][toX].canTravelFrom[(int)MoveDirection.NtoS];
+			}
+			return false; //not adjacent
 		}
 	}
 }
--- FFTools_Waypoint.cs
+++ FFTools_Waypoint.cs
@@ -2,70 +2,68 @@ using System;
 using System.Collections.Generic;
 
 namespace FFTools {
-	public class Waypoint : Location {
+	public enum MoveDirection {NtoS, StoN, EtoW, WtoE};
+	public class Waypoint {
 		//XYZ coordinates
-		//inherited public float x;	
-		//inherited public float y;	
-		//inherited public float z;	
+		public Location location;
 
 		//indicates whether it is possible to travel to location from direction
-		public bool NtoS;
-		public bool StoN;
-		public bool EtoW;
-		public bool WtoE;
+		public bool[] canTravelFrom = new bool[4];
+		//0: public bool NtoS;
+		//1: public bool StoN;
+		//2: public bool EtoW;
+		//3: public bool WtoE;
 
 		public Waypoint() {
-			this.x = 0;
-			this.y = 0;
-			this.z = 0;
+			this.location = new Location();
 
-			this.NtoS = true;
-			this.StoN = true;
-			this.EtoW = true;
-			this.WtoE = true;
+			this.canTravelFrom = new bool[] {true, true, true, true};
+			//this.NtoS = true;
+			//this.StoN = true;
+			//this.EtoW = true;
+			//this.WtoE = true;
 		}
 
 		public Waypoint(float x, float y, float z, bool ns, bool sn, bool ew, bool we) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
+			this.location.x = x;
+			this.location.y = y;
+			this.location.z = z;
 
-			this.NtoS = ns;
-			this.StoN = sn;
-			this.EtoW = ew;
-			this.WtoE = we;
+			this.canTravelFrom = new bool[] {ns, sn, ew, we};
+			//this.NtoS = ns;
+			//this.StoN = sn;
+			//this.EtoW = ew;
+			//this.WtoE = we;
 		}
 
 		public Waypoint(Location location) {
-			this.x = location.x;
-			this.y = location.y;
-			this.z = location.z;
+			this.location = location;
 
-			this.NtoS = true;
-			this.StoN = true;
-			this.EtoW = true;
-			this.WtoE = true;
+			this.canTravelFrom = new bool[] {true, true, true, true};
+			//this.NtoS = true;
+			//this.StoN = true;
+			//this.EtoW = true;
+			//this.WtoE = true;
 		}
 
 		public Waypoint(Location location, bool ns, bool sn, bool ew, bool we) {
-			this.x = location.x;
-			this.y = location.y;
-			this.z = location.z;
+			this.location = location;
 
-			this.NtoS = ns;
-			this.StoN = sn;
-			this.EtoW = ew;
-			this.WtoE = we;
+			this.canTravelFrom = new bool[] {ns, sn, ew, we};
+			//this.NtoS = ns;
+			//this.StoN = sn;
+			//this.EtoW = ew;
+			//this.WtoE = we;
 		}
 
 		public override string ToString() {
-			return "X: " + x + " | " +
-				   "Y: " + y + " | " +	
-				   "Z: " + z + " | " +
-				   "NtoS: " + NtoS + " | " +
-				   "StoN: " + StoN + " | " +
-				   "EtoW: " + EtoW + " | " +
-				   "WtoE: " + WtoE ;
+			return "X: " + location.x + " | " +
+				   "Y: " + location.y + " | " +	
+				   "Z: " + location.z + " | " +
+				   "NtoS: " + canTravelFrom[(int) MoveDirection.NtoS] + " | " +
+				   "StoN: " + canTravelFrom[(int) MoveDirection.StoN] + " | " +
+				   "EtoW: " + canTravelFrom[(int) MoveDirection.EtoW] + " | " +
+				   "WtoE: " + canTravelFrom[(int) MoveDirection.WtoE] ;
 		}
 	}
 }
