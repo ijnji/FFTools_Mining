d7fbf7d375e8af75f8c33ee7066d71c80997e68a
Ran Duan <rnduan@gmail.com>
2014-10-05 23:35:28 -0500

    Navigator class update

--- FFTools_Navigator.cs
+++ FFTools_Navigator.cs
@@ -8,9 +8,17 @@ using System.Windows.Forms;
 
 namespace FFTools {
     public class Navigator {
-        private static MemoryManager TheMemory;
+        // The MemoryManager is used for sending keypresses only.
+        private MemoryManager TheMemory = null;
+        private enum States {IDLE};
+        private States CurrentState = States.IDLE;
+        private Location TargetLoc = null;
         public Navigator(MemoryManager newTheMemory) {
             TheMemory = newTheMemory;
+        }
+        // Main loop is responsible for periodically calling update to refresh data.
+        public void update(Player thePlayer) {
+
         }
     } 
 }
